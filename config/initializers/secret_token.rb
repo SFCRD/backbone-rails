32e2031d25b216fc630763e1f27c0f55c93a3406
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-06-14 18:09:09 -0700

    hello git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneRails::Application.config.secret_token = 'adea61072bbb7d51b4b7bdb82614d2cbc5daa7fbb346f2e3904dba5ea5f057d9a5b912cdbd62bea2d87bd5d9a74d544abffdb00ef743135e3f2e37211a9eb1e6'
